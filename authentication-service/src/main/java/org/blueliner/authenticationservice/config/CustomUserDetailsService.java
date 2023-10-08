e2d30434a3bb78a2c5442ba912234902ce4f439d
nevels <neevels@mail.ru>
2023-10-08 20:36:23 +0300

    implement auth logic

---
+++ authentication-service/src/main/java/org/blueliner/authenticationservice/config/CustomUserDetailsService.java
@@ -0,0 +1,23 @@
+package org.blueliner.authenticationservice.config;
+
+import lombok.RequiredArgsConstructor;
+import org.blueliner.authenticationservice.repository.UserCredentialRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserCredentialRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return repository.findByEmail(username)
+                .map(CustomUserDetails::new)
+                .orElseThrow(() -> new UsernameNotFoundException("user not found with name :" + username));
+    }
+
+}
